cbcc8e0962ae359af263e48a948023ee9c21b363
Gustavo Alonso Gutierrez <136649737+GustavoAG7201@users.noreply.github.com>
2025-01-17 12:55:41 -0600

    17-Enero
    Creacion de nabvar basico con estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {InicioComponent} from './content/inicio/inicio.component';
+import { PortafolioComponent } from './content/portafolio/portafolio.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path: '', component: InicioComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'portafolio', component: PortafolioComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
