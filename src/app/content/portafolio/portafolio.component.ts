5be112725f78652a1ba87692dcf071b6fef3f353
Gustavo Alonso Gutierrez <136649737+GustavoAG7201@users.noreply.github.com>
2025-01-28 12:47:15 -0600

    28-Enero-2024 P2
    Practicas de TS

--- src/app/content/portafolio/portafolio.component.ts
+++ src/app/content/portafolio/portafolio.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrl: './portafolio.component.css'
 })
 export class PortafolioComponent {
+  // 1. Atributos/Variables/Tipos de datos
+  
 
+  //! Ejercicio 1. Mostrar un texto en consola
+  texto!: string 
+  nombre = "Gustavo"
+  apellido = "Alonso"
+
+  //* combinaciones/concatenar
+  nombre_completo = "Bienvenido! " + this.nombre + " " + this.apellido + "!"
+
+  // 2. Metodos
+  //* Este metodo se ejecuta al momento de iniciar la pagina
+  ngOnInit(){
+    this.texto = "Hola mundo!"
+    console.log(this.texto)
+
+
+    //! Ejercicio 2. Mostrar un texto en alerta
+    alert(this.texto)
+
+
+    //! Ejercicio 4. Mostrar dos textos concatenados
+    alert(this.nombre_completo)
+  }
 }
