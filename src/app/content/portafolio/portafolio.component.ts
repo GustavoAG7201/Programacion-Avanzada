cbde0502a65654ae29495b502d4cfa45b7f60ffc
Gustavo Alonso Gutierrez <136649737+GustavoAG7201@users.noreply.github.com>
2025-01-30 12:50:53 -0600

    30-Enero-2024
    Tabla en portafolio
    centrado de navbar
    (falta cambiar el color)

--- src/app/content/portafolio/portafolio.component.ts
+++ src/app/content/portafolio/portafolio.component.ts
@@ -8,27 +8,54 @@ import { Component } from '@angular/core';
 export class PortafolioComponent {
   // 1. Atributos/Variables/Tipos de datos
   
-
-  //! Ejercicio 1. Mostrar un texto en consola
   texto!: string 
-  nombre = "Gustavo"
-  apellido = "Alonso"
-
-  //* combinaciones/concatenar
-  nombre_completo = "Bienvenido! " + this.nombre + " " + this.apellido + "!"
+  nombre!: string
+  apellido!: string
+  nombre_completo!:string
+  num1!: number
+  num2!: number
+  resultado_suma!: number
+  resultado_resta!:number
+  resultado_multi!:number
+  resultado_div!:number
 
   // 2. Metodos
   //* Este metodo se ejecuta al momento de iniciar la pagina
   ngOnInit(){
+
+    this.nombre = "Gustavo"
+    this.apellido = "Alonso"
+    this.nombre_completo = "Bienvenido! " + this.nombre + " " + this.apellido + "!"
+
     this.texto = "Hola mundo!"
-    console.log(this.texto)
+    // console.log(this.texto)
 
 
     //! Ejercicio 2. Mostrar un texto en alerta
-    alert(this.texto)
+    // alert(this.texto)
 
 
     //! Ejercicio 4. Mostrar dos textos concatenados
-    alert(this.nombre_completo)
+    // alert(this.nombre_completo)
+
+
+    //!Operaciones
+    this.num1 = 10
+    this.num2 = 50
+    
+    //*Suma (Ejercicio 5)
+    this.resultado_suma =  this.num1 + this.num2
+
+    //*Resta
+    this.resultado_resta = this.num1 - this.num2
+
+    //*Multiplicacion
+    this.resultado_multi = this.num1 * this.num2
+
+    //*Division
+    this.resultado_div = this.num1 / this.num2
+
+
+
   }
 }
