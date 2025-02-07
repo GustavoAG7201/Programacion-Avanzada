660d94749f9788e88161f777a191d2139a7c73d6
Gustavo Alonso Gutierrez <136649737+GustavoAG7201@users.noreply.github.com>
2025-02-07 12:08:12 -0600

    7-Febrero-2025
    Formularios terminados
    Falta agregar los colores y cambiar la tipografia

--- src/app/content/portafolio/portafolio.component.ts
+++ src/app/content/portafolio/portafolio.component.ts
@@ -16,10 +16,10 @@ export class PortafolioComponent {
   nombre_completo!: string;
   num1!: number;
   num2!: number;
-  resultado_suma!: number;
-  resultado_resta!: number;
-  resultado_multi!: number;
-  resultado_div!: number;
+  // resultado_suma!: number;
+  // resultado_resta!: number;
+  // resultado_multi!: number;
+  // resultado_div!: number;
   gfarenheit!: number;
   gcelsius!: number;
   genero!: string;
@@ -28,59 +28,34 @@ export class PortafolioComponent {
   resultado_calificacion!: string;
   numero1!: number;
   numero2!: number;
-  resultado!: number;
+  operacion!: string;
+  resultado: number | string = '';
 
   // 2. Metodos
   //!Este metodo se ejecuta al momento de iniciar la pagina
   ngOnInit() {
     // this.genero = 'hombre';
-
     //!Operaciones
     // this.num1 = 10;
     // this.num2 = 50;
-
     // //*Suma (Ejercicio 5)
     // this.resultado_suma = this.num1 + this.num2;
-
     // //*Resta
     // this.resultado_resta = this.num1 - this.num2;
-
     // //*Multiplicacion
     // this.resultado_multi = this.num1 * this.num2;
-
     // //*Division
     // this.resultado_div = this.num1 / this.num2;
-
     //* Convertir grados farenheit a celcius
     // this.gfarenheit = 90;
     // this.gcelsius = ((this.gfarenheit - 32) * 5) / 9;
-
     //*Mostrar un mensaje dependiendo el genero
     // if (this.genero == 'hombre') {
     //   this.mensaje_genero = 'Bienvenido al curso!';
     // }
-
     // if (this.genero == 'mujer') {
     //   this.mensaje_genero = 'Bienvenida al curso!';
     // }
-
-    //*Pedir una calificacion y mostrar un mensaje segun la nota
-
-    this.calificacion = 100;
-
-    if (this.calificacion >= 0 && this.calificacion <= 30) {
-      this.resultado_calificacion = 'Muy ineficiente';
-    } else if (this.calificacion >= 31 && this.calificacion <= 50) {
-      this.resultado_calificacion = 'Insuficiente';
-    } else if (this.calificacion >= 51 && this.calificacion <= 60) {
-      this.resultado_calificacion = 'Suficiente';
-    } else if (this.calificacion >= 61 && this.calificacion <= 70) {
-      this.resultado_calificacion = 'Bien';
-    } else if (this.calificacion >= 71 && this.calificacion <= 90) {
-      this.resultado_calificacion = 'Notable';
-    } else if (this.calificacion >= 91 && this.calificacion <= 100) {
-      this.resultado_calificacion = 'Sobresaliente';
-    }
   }
 
   saludar() {
@@ -105,10 +80,24 @@ export class PortafolioComponent {
     }
   }
 
-  calificaciones() {}
+  clasificar_calificacion() {
+    if (this.calificacion >= 0 && this.calificacion <= 30) {
+      this.resultado_calificacion = 'Muy ineficiente';
+    } else if (this.calificacion >= 31 && this.calificacion <= 50) {
+      this.resultado_calificacion = 'Insuficiente';
+    } else if (this.calificacion >= 51 && this.calificacion <= 60) {
+      this.resultado_calificacion = 'Suficiente';
+    } else if (this.calificacion >= 61 && this.calificacion <= 70) {
+      this.resultado_calificacion = 'Bien';
+    } else if (this.calificacion >= 71 && this.calificacion <= 90) {
+      this.resultado_calificacion = 'Notable';
+    } else if (this.calificacion >= 91 && this.calificacion <= 100) {
+      this.resultado_calificacion = 'Sobresaliente';
+    }
+  }
 
-  operar(operacion: string) {
-    switch (operacion) {
+  operar() {
+    switch (this.operacion) {
       case 'suma':
         this.resultado = this.numero1 + this.numero2;
         break;
@@ -128,6 +117,8 @@ export class PortafolioComponent {
           this.resultado = this.numero1 / this.numero2;
         }
         break;
+      default:
+        this.resultado = 'Por favor, selecciona una opcion valida';
     }
   }
 }
