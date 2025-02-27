cf61820e88cb14d73f186571c072c2fd7f7c77a2
Gustavo Alonso Gutierrez <136649737+GustavoAG7201@users.noreply.github.com>
2025-02-27 12:47:21 -0600

    27/02/2024
    Se regreso todo a como estaba antes del examen
    Solo se dejo el componente creado

--- src/app/content/examen2/examen2.component.ts
+++ src/app/content/examen2/examen2.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-examen2',
   templateUrl: './examen2.component.html',
-  styleUrl: './examen2.component.css'
+  styleUrl: './examen2.component.css',
 })
-export class Examen2Component {
-
-}
+export class Examen2Component {}
